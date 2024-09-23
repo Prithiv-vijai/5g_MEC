22e8d4d4ce61dc07f0adab106346ccfd3608eefb
Prithiv <prithivvijai@gmail.com>
2024-09-24 00:17:27 +0530

    modified ppt for second review , still more

--- src/model.py
+++ src/model.py
@@ -7,10 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet, BayesianRidge
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.kernel_ridge import KernelRidge
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
